cc4669afe74f373cf0c1adf26e636960d7cadbee
QuisQuid1 <152250572+QuisQuid1@users.noreply.github.com>
2024-04-16 22:44:46 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вектора
+{ 
+    class Vector
+    {
+        public double X1 { get; }
+        public double Y1 { get; }
+        public double Z1 { get; }
+        public double X2 { get; }
+        public double Y2 { get; }
+        public double Z2 { get; }
+
+        public double[] Coordinates => new[] { X1, Y1, Z1, X2, Y2, Z2 };
+
+        public Vector(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            Z1 = z1;
+            X2 = x2;
+            Y2 = y2;
+            Z2 = z2;
+        }
+
+        public Vector(double[] coordinates)
+        {
+            X1 = coordinates[0];
+            Y1 = coordinates[1];
+            Z1 = coordinates[2];
+            X2 = coordinates[3];
+            Y2 = coordinates[4];
+            Z2 = coordinates[5];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace вектора
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector = new Vector(0, 0, 0, 0, 0, 0);
+            Vector v1 = new Vector(1, 2, 3, 4, 5, 6);
+            Vector v2 = new Vector(4, 5, 6, 7, 8, 9);
+            Vector sum = new Vector(
+            vector.Coordinates.Zip(v1.Coordinates, (v, w) => v + w).ToArray()
+            );
+            Vector difference = new Vector(
+                vector.Coordinates.Zip(v1.Coordinates, (v, w) => v - w).ToArray()
+            );
+            double dotProduct = vector.Coordinates.Zip(v1.Coordinates, (v, w) => v * w).Sum();
+
+            double magnitude = Math.Sqrt(v1.Coordinates.Select(v => v * v).Sum());
+
+            double cosineSimilarity = vector.Coordinates.Zip(v1.Coordinates, (v, w) => v * w).Sum()
+                                     / (Math.Sqrt(vector.Coordinates.Select(v => v * v).Sum())
+                                     * Math.Sqrt(v1.Coordinates.Select(w => w * w).Sum()));
+
+            Console.WriteLine("Сумма векторов: ({0}, {1}, {2}, {3}, {4}, {5})", sum.X1, sum.Y1, sum.Z1, sum.X2, sum.Y2, sum.Z2);
+            Console.WriteLine("Разность векторов: ({0}, {1}, {2}, {3}, {4}, {5})", difference.X1, difference.Y1, difference.Z1, difference.X2, difference.Y2, difference.Z2);
+            Console.WriteLine("Скалярное произведение векторов: {0}", dotProduct);
+            Console.WriteLine("Длина вектора v1: {0}", magnitude);
+            Console.WriteLine("Косинус угла между векторами: {0}", cosineSimilarity);
+            Console.ReadKey();
+        }
+    }
+}
